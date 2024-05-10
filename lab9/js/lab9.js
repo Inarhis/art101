95c7fd701133453be192a11feccfcb5215cf8d0a
Jianne <100251005+Inarhis@users.noreply.github.com>
2024-05-09 18:21:01 -0700

    fixed my layout

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -8,41 +8,11 @@
 */
 
 $(document).ready(function() {
-    // Add buttons to challenge, problems, reflection, and results sections
-    $("#challenge").append("<button id='button-challenge'>Make Special</button>");
-    $("#problems").append("<button id='button-problems'>Make Special</button>");
-    $("#reflection").append("<button id='button-reflection'>Make Special</button>");
-    $("#results").append("<button id='button-results'>Make Special</button>");
-
-    // Add click listeners to the buttons
-    $("#button-challenge").click(function() {
-        $("#challenge").toggleClass("special");
+    $(".special-sections").append("<button class='make-special'>Make Special</button>");
+    $(".make-special").click(function() {
+        $(this).parent().toggleClass("special").find("p").toggleClass("bold");
     });
-
-    $("#button-problems").click(function() {
-        $("#problems").toggleClass("special");
-    });
-
-    $("#button-reflection").click(function() {
-        $("#reflection").toggleClass("special");
-    });
-
-    $("#button-results").click(function() {
-        // Toggle the "special" class on the results section
-        $("#results").toggleClass("special");
-
-        // Check if the "special" class is added to the results section
-        if ($("#results").hasClass("special")) {
-            // If it is, append an image to the results section
-            $("#results").append("<img src='img/frog.png' alt='Frog'>");
-        } else {
-            // If not, remove the image from the results section
-            $("#results img").remove();
-        }
-    });
-
-    // Toggle the "special" class on all sections when the document is ready
-    $("section").each(function() {
-        $(this).toggleClass("special");
+    $("#results .make-special").click(function() {
+        $("#results").toggleClass("special").find("img").toggle();
     });
 });
