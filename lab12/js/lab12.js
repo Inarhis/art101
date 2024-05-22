5c6f13aec918455e7bcbf1a51b202a724410694e
Jianne <100251005+Inarhis@users.noreply.github.com>
2024-05-22 15:05:20 -0700

    did the lab

---
+++ lab12/js/lab12.js
@@ -0,0 +1,54 @@
+/*
+Lab 12: Sorting Hat
+
+   Author: Jianne Soto <Jiasoto@ucsc.edu>
+   Date: May 27 2024
+   License: Public Domain
+*/
+
+// Hogwarts house descriptions
+var houseDescriptions = {
+    Gryffindor: "Gryffindor values bravery, courage, and chivalry.",
+    Ravenclaw: "Ravenclaw values intelligence, wit, and wisdom.",
+    Slytherin: "Slytherin values ambition, cunning, and resourcefulness.",
+    Hufflepuff: "Hufflepuff values hard work, patience, loyalty, and fair play."
+};
+
+// Array of Hogwarts houses
+var houses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
+
+// Function to sort a name into a Hogwarts house
+function sortingHat(name) {
+    // Calculate the hash value
+    var hash = 0;
+    for (var i = 0; i < name.length; i++) {
+        hash += name.charCodeAt(i);
+    }
+    hash = Math.abs(hash);
+
+    // Use modulus to get the remainder when dividing by the number of houses
+    var mod = hash % houses.length;
+
+    // Get the name of the house
+    var house = houses[mod];
+
+    // Get the description of the house
+    var description = houseDescriptions[house];
+
+    // Return the name and description of the house
+    return {
+        name: house,
+        description: description
+    };
+}
+
+// Click listener for the button
+$('#button').click(function() {
+    // Get the value of the input field
+    var name = $('#input').val();
+
+    // Sort the name into a Hogwarts house
+    var house = sortingHat(name);
+
+    // Display the result
+    $('#output').appe
